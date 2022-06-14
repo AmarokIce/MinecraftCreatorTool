c6b3939dda7d9360534be1bab0121003ed8f9d48
Amarok <91077200+AmarokIce@users.noreply.github.com>
2022-06-15 00:33:19 +0800

    Fix GUIHelper!

--- src/python/icon/GIFHelper.py
+++ src/python/icon/GIFHelper.py
@@ -6,8 +6,14 @@ from tkinter.filedialog import askopenfilename
 from PIL import Image
 
 class GIFHelper:
-    mod_path = os.path.dirname(os.path.abspath(__file__))
-    __output_path = os.path.join(mod_path, "//output//")
+    __mod_path = os.path.dirname(os.path.abspath(__file__))
+    __output_path = os.path.join(__mod_path, "output\\")
+    
+    if os.path.exists(__output_path):
+        pass
+    else:
+        os.mkdir(__output_path)
+
     Img = Image.open(askopenfilename())
     print(Img)
     Img_size = Img.size
@@ -15,16 +21,15 @@ class GIFHelper:
     w, h = 16, 16
     x, y = 0, 0
     for i in range(n // 16):
-        region = Img.crop((x, y, x+w, y+h))
+        getImg = Img.crop((x, y, x+w, y+h))
         y += 16
-        region.save(__output_path + str(i) + ".png")
-
+        getImg.save(__output_path + str(i) + ".png")
 
 if __name__ == '__main__':
     classGIF = GIFHelper()
     try:
-        classGIF()
+        classGIF
     except:
         print('未知的問題。')
     else:
-        print('以輸出到路徑下output文件夾。')
+        print('以輸出到本文件夾下Output文件夾。')
